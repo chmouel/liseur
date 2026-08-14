6ee1b18491cc641c934634c3e1592b84096974b2
Chmouel Boudjnah <chmouel@chmouel.com>
2026-08-14 18:55:18 +0200

    chore: release v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.chmouel.liseur"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
-        versionName = "0.6.0"
+        versionCode = 12
+        versionName = "0.7.0"
     }
 
     signingConfigs {
