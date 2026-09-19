3e165571660244fa136cadfff39d209fbfe308f4
Chmouel Boudjnah <chmouel@chmouel.com>
2026-09-19 12:14:04 +0200

    chore: release candidate v0.18.0-rc.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.chmouel.liseur"
         minSdk = 26
         targetSdk = 37
-        versionCode = 41
-        versionName = "0.17.0"
+        versionCode = 42
+        versionName = "0.18.0-rc.1"
     }
 
     signingConfigs {
